47822a2f8aeb7daea818cb761be25cd44b81ea3d
Aryan Chauhan <chauhanaryan31801@gmail.com>
2025-10-02 11:56:24 +0530

    Create reduction1.cu.cpp

---
+++ reduction1.cu.cpp
@@ -0,0 +1,17 @@
+__global__ void reduce1(int *g_idata, int *g_odata) {
+  extern __shared__ int sdata[];
+  
+  unsigned int tid = threadIdx.x;
+  unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+  sdata[tid] = g_idata[i];
+  __syncthreads();
+  
+  for (unsigned int s=1; s < blockDim.x; s *= 2) {
+    if (tid % (2*s) == 0) {
+      sdata[tid] += sdata[tid + s];
+  }
+  __syncthreads();
+  }
+  
+  if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+}
