0c8ee20dae6f95fa1421429b464d9de5e6656a58
Aryan Chauhan <chauhanaryan31801@gmail.com>
2025-10-03 10:27:43 +0530

    Update reduction3.cu

--- reduction3.cu
+++ reduction3.cu
@@ -1,24 +1,35 @@
-#include<cuda_runtime.h>
+#include <cuda_runtime.h>
 
 #define THREADS_PER_BLOCK 256
 
-__global__ void reduction3(const float* input, float* output, int N){
-  __shared__ float smem[THREADS_PER_BLOCK};
+inline int cdiv(int a, int b){ return (a + b - 1)/b; }
 
-  unsigned int tid = threadIdx.x;
-  unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void reduction_kernel(const float* input, float* output, int N){
+    __shared__ float smem[THREADS_PER_BLOCK];
+    
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    float sum = 0.0f;
+    if(i<N) sum = input[i];
+    if(i+ blockDim.x < N) sum += input[i + blockDim.x];
+    smem[tid] = sum;
+    __syncthreads();
 
-  smem[tid] = (tid < N) ? input[tid] : 0.0f;
-  __syncThreads();
-  
-  for(unsigned int s = blockDim.x/2; s>0; s>>=1){
-    int index = s * 2 * tid;
+    for(unsigned int s = blockDim.x/2; s>0; s>>=1){
     if(tid < s){
-      smem[tid] += smem[tid + index];
+        smem[tid] += smem[tid +s];
     }
-    __syncThreads();
-  }
-  if(tid==0){
+    __syncthreads();
+    }
+    if(tid==0){
     output[blockIdx.x] = smem[0];
-  }
+    }
+}
+
+// input, output are device pointers; output must have at least "blocks" elements
+extern "C" void solve(const float* input, float* output, int N) {
+    dim3 threads(THREADS_PER_BLOCK);
+    // each thread can load up to 2 elements
+    dim3 blocks((N + threads.x * 2 - 1) / (threads.x * 2));
+    reduction_kernel<<<blocks, threads>>>(input, output, N);
 }
