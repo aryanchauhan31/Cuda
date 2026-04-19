93a91eee003fa06163a3184624925f77295ff412
Aryan Chauhan <chauhanaryan31801@gmail.com>
2026-04-19 15:01:30 -0400

    Create prefix_sum.cu
    Example 1:
    Input: [1.0, 2.0, 3.0, 4.0]
    Output: [1.0, 3.0, 6.0, 10.0]
    Example 2:
    Input: [5.0, -2.0, 3.0, 1.0, -4.0]
    Output: [5.0, 3.0, 6.0, 7.0, 3.0]
    
    time = 0.62ms

---
+++ prefix_sum.cu
@@ -0,0 +1,59 @@
+#include<cuda_runtime.h>
+
+__global__ void block_scan_kernel(const float* input, const float* output, float* block_sums, int N){
+  extern __shared__ double shared[];
+  int tid = threadIdx.x;
+  int gid = blockDim.x * blockIdx.x + tid;
+
+  shared[tid] = (gid < N) ? (double)input[tid] : 0.0;
+  __syncthreads();
+
+  for(int stride = 1; stride < blockDim.x; stride <<=1){
+    double temp = (tid >= stride) ? shared[tid - stride] : 0.0;
+    __syncthreads();
+    shared[tid] += temp;
+    __syncthreads();
+  }
+  if (gid < N) output[gid] = (float)shared[tid];
+  if (block_sums != nullptr && tid == blockDim.x - 1) {
+        block_sums[blockIdx.x] = (float)shared[tid];
+  }
+}
+
+__global__ void add_block_offsets_kernel(float* output, const float* block_sums, int N) {
+    int gid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (blockIdx.x > 0 && gid < N) {
+        output[gid] += block_sums[blockIdx.x - 1];
+    }
+}
+
+void scan(const float* input, float* output, int N) {
+    if (N <= 1024) {
+        int threads = 1;
+        while (threads < N) threads <<= 1;
+        threads = min(threads, 1024);
+        // double uses 8 bytes instead of 4
+        block_scan_kernel<<<1, threads, threads * sizeof(double)>>>(input, output, nullptr, N);
+        return;
+    }
+
+    int threads = 1024;
+    int blocks = (N + threads - 1) / threads;
+
+    float* block_sums;
+    float* scanned_block_sums;
+    cudaMalloc(&block_sums, blocks * sizeof(float));
+    cudaMalloc(&scanned_block_sums, blocks * sizeof(float));
+
+    block_scan_kernel<<<blocks, threads, threads * sizeof(double)>>>(input, output, block_sums, N);
+    scan(block_sums, scanned_block_sums, blocks);
+    add_block_offsets_kernel<<<blocks, threads>>>(output, scanned_block_sums, N);
+
+    cudaFree(block_sums);
+    cudaFree(scanned_block_sums);
+}
+
+extern "C" void solve(const float* input, float* output, int N) {
+    scan(input, output, N);
+    cudaDeviceSynchronize();
+}
